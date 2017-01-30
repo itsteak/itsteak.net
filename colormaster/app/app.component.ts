3387b0eb4c57e6a922c1436a8275e41864f2ea88
신연식 <shin.yeonsik@sualab.com>
2017-01-31 06:10:40 +0900

    initial codes, import @kolory/color-utilities

---
+++ colormaster/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { ColorUtilities } from '@kolory/color-utilities/src/library';
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/colorview.html'
+})
+export class AppComponent  { 
+    color = ''; 
+
+    rgbInputErrMsg = '';
+    hexInputErrMsg = '';
+
+    getColor(){ return this.color; }
+
+    setHexColor(hex:string){
+        const colorUtil = new ColorUtilities();
+
+        if (hex.length == 0){
+            this.hexInputErrMsg = '';
+        } else if (hex.length > 0 && !colorUtil.isValidHexColor(hex)){
+            this.hexInputErrMsg = "Wrong HEX format";
+            return;
+        }
+
+        this.color = hex;
+    }
+
+    setRgbColor(rgb:string){
+        const colorUtil = new ColorUtilities();
+
+        if (rgb.length == 0){
+            this.rgbInputErrMsg = '';
+        } else if (rgb.length > 0 && !colorUtil.isValidRgbColor(rgb)){
+            this.rgbInputErrMsg = "Wrong RGB format";
+            return;
+        }
+
+        this.color = rgb;
+    }
+}
