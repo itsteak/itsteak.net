9fa08cc6b50b98ad22acac6c759cb697b47208f9
신연식 <shin.yeonsik@sualab.com>
2017-02-12 23:50:46 +0900

    succeeded to contruct project structure with angular2-webpack-starter

--- colormaster/src/app/app.component.ts
+++ colormaster/src/app/app.component.ts
@@ -1,42 +1,76 @@
-import { Component } from '@angular/core';
+/*
+ * Angular 2 decorators and services
+ */
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
+import { AppState } from './app.service';
 import { ColorUtilities } from '@kolory/color-utilities/src/library';
 
-
+/*
+ * App Component
+ * Top Level Component
+ */
 @Component({
-  selector: 'my-app',
-  templateUrl: '../src/app/app.component.html'
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./app.component.css'],
+  templateUrl: 'app.component.html'
 })
-export class AppComponent  { 
-    color = ''; 
+export class AppComponent {
+    public rgbInputErrMsg = '';
+    public hexInputErrMsg = '';
 
-    rgbInputErrMsg = '';
-    hexInputErrMsg = '';
+    private color = '';
 
-    getColor(){ return this.color; }
+    public getColor() { return this.color; }
 
-    setHexColor(hex:string){
+    public setHexColor(hex: string) {
         const colorUtil = new ColorUtilities();
 
-        if (hex.length == 0){
+        if (hex.length === 0) {
             this.hexInputErrMsg = '';
-        } else if (hex.length > 0 && !colorUtil.isValidHexColor(hex)){
-            this.hexInputErrMsg = "Wrong HEX format";
+        } else if (hex.length > 0 && !colorUtil.isValidHexColor(hex)) {
+            this.hexInputErrMsg = 'Wrong HEX format';
             return;
         }
 
         this.color = hex;
     }
 
-    setRgbColor(rgb:string){
+    public setRgbColor(rgb: string) {
         const colorUtil = new ColorUtilities();
 
-        if (rgb.length == 0){
+        if (rgb.length === 0) {
             this.rgbInputErrMsg = '';
-        } else if (rgb.length > 0 && !colorUtil.isValidRgbColor(rgb)){
-            this.rgbInputErrMsg = "Wrong RGB format";
+        } else if (rgb.length > 0 && !colorUtil.isValidRgbColor(rgb)) {
+            this.rgbInputErrMsg = 'Wrong RGB format';
             return;
         }
 
         this.color = rgb;
     }
+
+  /*public angularclassLogo = 'assets/img/angularclass-avatar.png';
+  public name = 'Angular 2 Webpack Starter';
+  public url = 'https://twitter.com/AngularClass';
+
+  constructor(
+    public appState: AppState
+  ) {}
+
+  public ngOnInit() {
+    console.log('Initial App State', this.appState.state);
+  }*/
+
 }
+
+/*
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ */
