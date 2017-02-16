f5b3a7ee3d9128f475cb87df27e597dbcbde640f
신연식 <shin.yeonsik@sualab.com>
2017-02-17 05:51:15 +0900

    remove warnings

--- colormaster/src/app/app.module.ts
+++ colormaster/src/app/app.module.ts
@@ -19,18 +19,18 @@ import {
  * Platform and Environment providers/directives/pipes
  */
 import { ENV_PROVIDERS } from './environment';
-//import { ROUTES } from './app.routes';
+// import { ROUTES } from './app.routes';
 // App is our top level component
 import { AppComponent } from './app.component';
 import { APP_RESOLVER_PROVIDERS } from './app.resolver';
 import { AppState, InternalStateType } from './app.service';
-//import { HomeComponent } from './home';
-//import { AboutComponent } from './about';
-//import { NoContentComponent } from './no-content';
-//import { XLargeDirective } from './home/x-large';
+// import { HomeComponent } from './home';
+// import { AboutComponent } from './about';
+// import { NoContentComponent } from './no-content';
+// import { XLargeDirective } from './home/x-large';
 
-//import '../styles/styles.scss';
-//import '../styles/headings.css';
+// import '../styles/styles.scss';
+// import '../styles/headings.css';
 
 // Application wide providers
 const APP_PROVIDERS = [
@@ -51,16 +51,16 @@ type StoreType = {
   bootstrap: [ AppComponent ],
   declarations: [
     AppComponent,
-    //AboutComponent,
-    //HomeComponent,
-    //NoContentComponent,
-    //XLargeDirective
+    // AboutComponent,
+    // HomeComponent,
+    // NoContentComponent,
+    // XLargeDirective
   ],
   imports: [ // import Angular's modules
     BrowserModule,
     FormsModule,
     HttpModule,
-    //RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules })
+    // RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules })
   ],
   providers: [ // expose our Services and Providers into Angular's dependency injection
     ENV_PROVIDERS,
